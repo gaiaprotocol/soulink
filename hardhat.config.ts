e004cda759897068fabd3b9ac386bb5c23b5e13d
TheGreatHB <kavanaboy@gmail.com>
2022-07-18 00:01:43 +0900

    Set foundation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,31 +1,32 @@
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
 import "dotenv/config";
-import "hardhat-typechain";
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomicfoundation/hardhat-chai-matchers";
+import "@typechain/hardhat";
 import { HardhatUserConfig } from "hardhat/types";
+import "@nomiclabs/hardhat-solhint";
+import "solidity-coverage";
+// import "hardhat-gas-reporter";
+import "hardhat-tracer";
 
 const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [{
-      version: "0.8.9",
-      settings: {
-        optimizer: {
-          enabled: true,
-          runs: 200,
-        },
-      },
-    }]
-  },
-  networks: {
-    mainnet: {
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.ADMIN || ''],
-      chainId: 1,
+    solidity: {
+        compilers: [
+            {
+                version: "0.8.14",
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 200,
+                    },
+                },
+            },
+        ],
     },
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  },
+    networks: {},
+    etherscan: {
+        apiKey: process.env.ETHERSCAN_API_KEY,
+    },
 };
 
 export default config;
