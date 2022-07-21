56ba19e22d56941651ce76471a34f5612616c9f8
TheGreatHB <hb@thegreathb.com>
2022-07-21 21:18:48 +0900

    Housekeeping

--- contracts/SoulinkMinter.sol
+++ contracts/SoulinkMinter.sol
@@ -25,30 +25,34 @@ contract SoulinkMinter is Ownable, ISoulinkMinter {
     }
 
     function setFeeTo(address _feeTo) external onlyOwner {
+        require(feeTo != _feeTo, "UNCHANGED");
         feeTo = _feeTo;
         emit SetFeeTo(_feeTo);
     }
 
     function setLimit(uint96 _limit) external onlyOwner {
+        require(limit != _limit, "UNCHANGED");
         limit = _limit;
         emit SetLimit(_limit);
     }
 
     function setMintPrice(uint96 _price) external onlyOwner {
+        require(mintPrice != _price, "UNCHANGED");
         mintPrice = _price;
         emit SetMintPrice(_price);
     }
 
     function setDiscountDB(address db) external onlyOwner {
+        require(discountDB != db, "UNCHANGED");
         discountDB = db;
-        // emit SetDiscountDB(db);
+        emit SetDiscountDB(db);
     }
 
     function mint(bool discount, bytes calldata data) public payable returns (uint256 id) {
-        require(soulink.totalSupply() < limit, "SoulinkMinter: Limit exceeded");
+        require(soulink.totalSupply() < limit, "LIMIT_EXCEEDED");
         uint256 _mintPrice = mintPrice;
         if (discount) {
-            require(discountDB != address(0), "No discountDB");
+            require(discountDB != address(0), "NO_DISCOUNTDB");
             uint16 dcRate = IDiscountDB(discountDB).getDiscountRate(msg.sender, data);
             _mintPrice = (_mintPrice * (10000 - dcRate)) / 10000;
         }
