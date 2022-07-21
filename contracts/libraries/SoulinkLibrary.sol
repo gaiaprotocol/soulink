4cb797401ec14e4326738f2697fd293b6aece37d
TheGreatHB <hb@thegreathb.com>
2022-07-22 04:08:23 +0900

    Update SoulinkLibrary.sol

--- contracts/libraries/SoulinkLibrary.sol
+++ contracts/libraries/SoulinkLibrary.sol
@@ -9,8 +9,8 @@ library SoulinkLibrary {
     }
 
     function _sort(uint256 idA, uint256 idB) internal pure returns (uint256 id0, uint256 id1) {
-        require(idA != idB, "UniswapV2Library: IDENTICAL_ADDRESSES");
+        require(idA != idB, "IDENTICAL_ADDRESSES");
         (id0, id1) = idA < idB ? (idA, idB) : (idB, idA);
-        require(id0 != uint256(0), "UniswapV2Library: ZERO_ADDRESS");
+        require(id0 != uint256(0), "ZERO_ADDRESS");
     }
 }
