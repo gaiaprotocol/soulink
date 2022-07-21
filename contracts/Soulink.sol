8038a6fa885a4e5ebf36fac179facf8ed7929f12
TheGreatHB <hb@thegreathb.com>
2022-07-21 21:19:42 +0900

    Update Soulink

--- contracts/Soulink.sol
+++ contracts/Soulink.sol
@@ -2,26 +2,13 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
-import "@openzeppelin/contracts/utils/introspection/ERC165.sol";
 import "./libraries/SoulinkLibrary.sol";
-import "./interfaces/IERC721Metadata.sol";
+import "./standards/SoulBoundToken.sol";
+import "./interfaces/ISoulink.sol";
 
-// import "./interfaces/ISoulink.sol";
-
-contract Soulink is Ownable, ERC165, EIP712, IERC721Metadata {
-    using Strings for uint256;
-
-    event Transfer(address indexed from, address indexed to, uint256 indexed tokenId);
-
-    string private _name = "Soulink";
-    string private _symbol = "SL";
-
-    mapping(uint256 => address) private _owners;
-    mapping(address => uint256) private _balances;
-
+contract Soulink is Ownable, SoulBoundToken, EIP712, ISoulink {
     uint128 private _totalSupply;
     uint128 private _burnCount;
 
@@ -35,133 +22,39 @@ contract Soulink is Ownable, ERC165, EIP712, IERC721Metadata {
 
     string internal __baseURI;
 
-    constructor() EIP712(_name, "1") {
+    constructor() SoulBoundToken("Soulink", "SL") EIP712("Soulink", "1") {
         isMinter[msg.sender] = true;
 
         __baseURI = "https://api.soul.ink/metadata/";
     }
 
-    function _baseURI() internal view returns (string memory) {
-        return __baseURI;
-    }
-
+    //ownership functions
     function setBaseURI(string calldata baseURI_) external onlyOwner {
         __baseURI = baseURI_;
-        // emit SetBaseURI(baseURI_);
-    }
-
-    function supportsInterface(bytes4 interfaceId) public view override returns (bool) {
-        return interfaceId == type(IERC721Metadata).interfaceId || super.supportsInterface(interfaceId);
-    }
-
-    function DOMAIN_SEPARATOR() public view returns (bytes32) {
-        return _domainSeparatorV4();
-    }
-
-    function balanceOf(address owner) public view returns (uint256) {
-        require(owner != address(0), "ERC721: address zero is not a valid owner");
-        return _balances[owner];
-    }
-
-    function ownerOf(uint256 tokenId) public view returns (address) {
-        address owner = _owners[tokenId];
-        require(owner != address(0), "ERC721: invalid token ID");
-        return owner;
-    }
-
-    function name() public view returns (string memory) {
-        return _name;
-    }
-
-    function symbol() public view returns (string memory) {
-        return _symbol;
-    }
-
-    function totalSupply() public view returns (uint256) {
-        return _totalSupply - _burnCount;
-    }
-
-    function _exists(uint256 tokenId) internal view returns (bool) {
-        return _owners[tokenId] != address(0);
-    }
-
-    function _requireMinted(uint256 tokenId) internal view {
-        require(_exists(tokenId), "ERC721: invalid token ID");
-    }
-
-    function tokenURI(uint256 tokenId) public view returns (string memory) {
-        _requireMinted(tokenId);
-
-        string memory baseURI = _baseURI();
-        return bytes(baseURI).length > 0 ? string(abi.encodePacked(baseURI, tokenId.toString())) : "";
-    }
-
-    function _mint(address to, uint256 tokenId) internal {
-        require(to != address(0), "ERC721: mint to the zero address");
-        require(!_exists(tokenId), "ERC721: token already minted");
-
-        _balances[to] = 1;
-        _owners[tokenId] = to;
-        _totalSupply++;
-
-        emit Transfer(address(0), to, tokenId);
-    }
-
-    function _burn(uint256 tokenId) internal {
-        address owner = ownerOf(tokenId);
-
-        delete _balances[owner];
-        delete _owners[tokenId];
-        _burnCount++;
-
-        emit Transfer(owner, address(0), tokenId);
+        emit SetBaseURI(baseURI_);
     }
 
     function setMinter(address target, bool _isMinter) external onlyOwner {
         require(isMinter[target] != _isMinter, "Soulink: Permission not changed");
         isMinter[target] = _isMinter;
-        // emit SetMinter(target, _isMinter);
+        emit SetMinter(target, _isMinter);
     }
 
     function updateSigNotUsable(bytes32 sigHash) external onlyOwner {
         _notUsableSig[sigHash] = true;
     }
 
-    function cancelLinkSig(
-        uint256 targetId,
-        uint256 deadline,
-        bytes calldata sig
-    ) external {
-        bytes32 hash = _hashTypedDataV4(keccak256(abi.encode(_REQUESTLINK_TYPEHASH, targetId, deadline)));
-        SignatureChecker.isValidSignatureNow(msg.sender, hash, sig);
+    //external view/pure functions
+    function DOMAIN_SEPARATOR() public view returns (bytes32) {
+        return _domainSeparatorV4();
+    }
 
-        bytes32 sigHash = keccak256(sig);
-        _requireUsable(sigHash);
-        _notUsableSig[sigHash] = true;
-        // emit CancelLinkSig(msg.sender, targetId, deadline);
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply - _burnCount;
     }
 
     function getTokenId(address owner) public pure returns (uint256) {
         return SoulinkLibrary._getTokenId(owner);
-    }
-
-    function mint(address to) external returns (uint256 tokenId) {
-        require(isMinter[msg.sender], "Soulink: Forbidden");
-        require(balanceOf(to) == 0, "can have only 1 token");
-        tokenId = getTokenId(to);
-        _mint(to, tokenId);
-        _internalId[tokenId] = _totalSupply; //_internalId starts from 1
-    }
-
-    function burn(uint256 tokenId) external {
-        require(getTokenId(msg.sender) == tokenId, "ERC721: caller is not token owner");
-        _burn(tokenId);
-        delete _internalId[tokenId];
-        // emit ResetLink(tokenId);
-    }
-
-    function _requireUsable(bytes32 sig) internal view {
-        require(!_notUsableSig[sig], "USED_SIGNATURE");
     }
 
     function isLinked(uint256 id0, uint256 id1) external view returns (bool) {
@@ -169,11 +62,38 @@ contract Soulink is Ownable, ERC165, EIP712, IERC721Metadata {
         return _isLinked[iId0][iId1];
     }
 
+    //internal view/pure functions
+    function _baseURI() internal view override returns (string memory) {
+        return __baseURI;
+    }
+
+    function _requireUsable(bytes32 sig) internal view {
+        require(!_notUsableSig[sig], "USED_SIGNATURE");
+    }
+
     function _getInternalIds(uint256 id0, uint256 id1) internal view returns (uint256 iId0, uint256 iId1) {
         _requireMinted(id0);
         _requireMinted(id1);
 
         (iId0, iId1) = SoulinkLibrary._sort(_internalId[id0], _internalId[id1]);
+    }
+
+    //external functions
+    function mint(address to) external returns (uint256 tokenId) {
+        require(isMinter[msg.sender], "Soulink: Forbidden");
+        require(balanceOf(to) == 0, "can have only 1 token");
+        tokenId = getTokenId(to);
+        _mint(to, tokenId);
+        _totalSupply++;
+        _internalId[tokenId] = _totalSupply; //_internalId starts from 1
+    }
+
+    function burn(uint256 tokenId) external {
+        require(getTokenId(msg.sender) == tokenId, "ERC721: caller is not token owner");
+        _burn(tokenId);
+        _burnCount++;
+        delete _internalId[tokenId];
+        emit ResetLink(tokenId);
     }
 
     /**
@@ -204,7 +124,7 @@ contract Soulink is Ownable, ERC165, EIP712, IERC721Metadata {
         (uint256 iId0, uint256 iId1) = _getInternalIds(myId, targetId);
         require(!_isLinked[iId0][iId1], "ALREADY_LINKED");
         _isLinked[iId0][iId1] = true;
-        // emit SetLink(myId, targetId);
+        emit SetLink(myId, targetId);
     }
 
     function breakLink(uint256 targetId) external {
@@ -212,6 +132,20 @@ contract Soulink is Ownable, ERC165, EIP712, IERC721Metadata {
         (uint256 iId0, uint256 iId1) = _getInternalIds(myId, targetId);
         require(_isLinked[iId0][iId1], "NOT_LINKED");
         delete _isLinked[iId0][iId1];
-        // emit BreakLink(myId, targetId);
+        emit BreakLink(myId, targetId);
+    }
+
+    function cancelLinkSig(
+        uint256 targetId,
+        uint256 deadline,
+        bytes calldata sig
+    ) external {
+        bytes32 hash = _hashTypedDataV4(keccak256(abi.encode(_REQUESTLINK_TYPEHASH, targetId, deadline)));
+        SignatureChecker.isValidSignatureNow(msg.sender, hash, sig);
+
+        bytes32 sigHash = keccak256(sig);
+        _requireUsable(sigHash);
+        _notUsableSig[sigHash] = true;
+        emit CancelLinkSig(msg.sender, targetId, deadline);
     }
 }
--- contracts/interfaces/ISoulink.sol
+++ contracts/interfaces/ISoulink.sol
@@ -1,20 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-import "@openzeppelin/contracts/interfaces/IERC721Metadata.sol";
+import "./ISoulBoundToken.sol";
 
-// import "./IERC4973.sol";
-
-interface ISoulink is IERC721Metadata {
-    event SetMinter(address indexed target, bool indexed isMinter);
-    event SetContractURI(string uri);
+interface ISoulink is ISoulBoundToken {
     event SetBaseURI(string uri);
+    event SetMinter(address indexed target, bool indexed isMinter);
+    event CancelLinkSig(address indexed caller, uint256 indexed targetId, uint256 deadline);
+    event ResetLink(uint256 indexed tokenId);
+    event SetLink(uint256 indexed id0, uint256 indexed id1);
+    event BreakLink(uint256 indexed id0, uint256 indexed id1);
+
+    function DOMAIN_SEPARATOR() external view returns (bytes32);
 
     function totalSupply() external view returns (uint256);
 
     function isMinter(address target) external view returns (bool);
 
     function getTokenId(address owner) external pure returns (uint256);
+
+    function cancelLinkSig(
+        uint256 targetId,
+        uint256 deadline,
+        bytes calldata sig
+    ) external;
 
     function mint(address to) external returns (uint256 id);
 
