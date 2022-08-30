c6048678db9e8cc6a3890a1e339461e558e4f839
TheGreatHB <hb@thegreathb.com>
2022-08-30 17:09:02 +0900

    Add ERC1155 holder discount system to DiscountDB
    DiscountDBV0 -> DiscountDBV1

--- contracts/interfaces/IDiscountDB.sol
+++ contracts/interfaces/IDiscountDB.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.15;
 
 interface IDiscountDB {
     event UpdateNFTDiscountRate(address nft, uint16 discountRate);
+    event UpdateERC1155DiscountRate(address token, uint256 tokenId, uint16 discountRate);
     event UpdateUserDiscountRate(address user, uint16 discountRate);
 
     function getDiscountRate(address target, bytes calldata data) external view returns (uint16);
